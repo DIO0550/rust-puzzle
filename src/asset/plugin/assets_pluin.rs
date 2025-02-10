d898644ecd2771cd809cd9b9d4b83ef22dee1bd1
DIO0550 <diot12345@yahoo.co.jp>
2025-02-10 21:41:53 +0900

    Assetsのロード確認

--- src/asset/plugin/assets_pluin.rs
+++ src/asset/plugin/assets_pluin.rs
@@ -1,11 +1,26 @@
-use bevy::app::{App, Plugin, PreStartup};
+use bevy::{
+    app::{App, Plugin, PreStartup, Update},
+    ecs::schedule::{common_conditions::in_state, IntoSystemConfigs},
+};
 
-use crate::asset::system::assets_sysmtem::load_assets;
+use crate::{
+    asset::{
+        image::image_assets::ImageAssets,
+        state::asset_load_state::AssetLoadState,
+        system::assets_sysmtem::{check_assets_ready, load_assets},
+    },
+    piece::component::animal_piece::piece_image::PieceImage,
+};
 
 pub struct AssetsPlugin;
 
 impl Plugin for AssetsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(PreStartup, load_assets);
+        app.add_state::<AssetLoadState>()
+            .add_systems(PreStartup, load_assets)
+            .add_systems(
+                Update,
+                (check_assets_ready).run_if(in_state(AssetLoadState::Loading)),
+            );
     }
 }
--- src/asset/system/assets_sysmtem.rs
+++ src/asset/system/assets_sysmtem.rs
@@ -1,12 +1,19 @@
 use bevy::{
     asset::AssetServer,
-    ecs::system::{Commands, Res},
+    ecs::{
+        schedule::{NextState, State},
+        system::{Commands, Res, ResMut},
+    },
 };
 
-use crate::asset::{
-    asset::AssetLoadTrait,
-    font::font_assets::FontAssets,
-    image::{image_assets::ImageAssets, piece_image_assets::PieceImageAssets},
+use crate::{
+    asset::{
+        asset::AssetLoadTrait,
+        font::font_assets::FontAssets,
+        image::{image_assets::ImageAssets, piece_image_assets::PieceImageAssets},
+        state::asset_load_state::AssetLoadState,
+    },
+    game::state::{game_page_state::GamePageState, game_state::GameState},
 };
 
 use super::{font_loader_system::load_font_assets, image_loader_system::load_image_assets};
@@ -18,14 +25,35 @@ pub fn load_assets(mut commands: Commands, asset_server: Res<AssetServer>) {
 
 pub fn check_assets_ready(
     asset_server: Res<AssetServer>,
-    piece_image_assets: Res<PieceImageAssets>,
-    image_assets: Res<ImageAssets>,
-    font_assets: Res<FontAssets>,
+    assets: (
+        Option<Res<PieceImageAssets>>,
+        Option<Res<ImageAssets>>,
+        Option<Res<FontAssets>>,
+    ),
+    mut assets_load_state: ResMut<NextState<AssetLoadState>>,
+    mut game_page_state: ResMut<NextState<GamePageState>>,
 ) {
-    if (piece_image_assets.is_loaded(&asset_server)
-        && image_assets.is_loaded(&asset_server)
-        && font_assets.is_loaded(&asset_server))
-    {
-        // TODO: ここでアセットのロードが完了したことを通知する
+    let all_loaded = [
+        assets
+            .0
+            .map(|a| a.is_loaded(&asset_server))
+            .unwrap_or(false),
+        assets
+            .1
+            .map(|a| a.is_loaded(&asset_server))
+            .unwrap_or(false),
+        assets
+            .2
+            .map(|a| a.is_loaded(&asset_server))
+            .unwrap_or(false),
+    ]
+    .iter()
+    .all(|loaded| *loaded);
+
+    if !all_loaded {
+        return;
     }
+
+    assets_load_state.set(AssetLoadState::Loaded);
+    game_page_state.set(GamePageState::Title);
 }
